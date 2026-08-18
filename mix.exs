22f59c2cd74456f00774e588bac37e9681a4177e
Paweł Świątkowski <katafrakt@vivaldi.net>
2026-08-18 13:25:30 +0200

    sobelow failure

--- mix.exs
+++ mix.exs
@@ -21,8 +21,7 @@ defmodule Foo.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:sobelow, git: "https://github.com/katafrakt/sobelow.git", branch: "github-workflow-annotations", only: [:dev, :test], runtime: false}
     ]
   end
 end
